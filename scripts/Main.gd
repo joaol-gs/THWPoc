545b93762b1265b81e5ff3a47e1d6835664d79b3
Alunos <mail@mail.com>
2019-06-24 08:05:54 -0300

    update da manha

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,14 +6,17 @@ var next_level = pre_play.instance()
 func _ready():
 	pass 
 
-#func _process(delta):
-#	pass
-
-func _on_Start_pressed():
-	$Panel.visible = false
-	get_tree().get_root().add_child(next_level)
-
-
 func _on_TouchScreenButton_pressed():
-#	$.visible = false
+	_esconde()
 	get_tree().get_root().add_child(next_level)
+
+func _esconde():
+	$Menu/btnStart.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer2.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer3.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer4.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer5.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer6.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer7.visible = false
+	$Menu/ParallaxBackground/ParallaxLayer8.visible = false
